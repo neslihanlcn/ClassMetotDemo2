baf485fb15c36d5fda1682d65702420b0db5f0ff
Neslihan LAÇİN <neslihan.lacin18@ogr.atauni.edu.tr>
2021-07-28 00:20:30 +0300

    Add project files.

---
+++ ClassMetotDemo/Musteri.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class Musteri
+    {
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+        public int KimlikNumarasi { get; set; }
+        public int TelefonNumarasi { get; set; }
+        public string meslek { get; set; }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Adi = "selin";
+            musteri1.Soyadi = "yıldız";
+            musteri1.meslek = "öğretmen";
+            musteri1.TelefonNumarasi = 123456789;
+            musteri1.KimlikNumarasi = 111111111;
+
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Adi = "bahar";
+            musteri2.Soyadi = "güler";
+            musteri2.meslek = "pilot";
+            musteri2.TelefonNumarasi = 4567633;
+            musteri2.KimlikNumarasi = 22222222;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Adi = "demir";
+            musteri3.Soyadi = "solmaz";
+            musteri3.meslek = "esnaf";
+            musteri3.TelefonNumarasi = 345322345;
+            musteri3.KimlikNumarasi = 3333333;
+
+            Musteri musteri4 = new Musteri();
+            musteri4.Adi = "ozan";
+            musteri4.Soyadi = "bulut";
+            musteri4.meslek = "doktor";
+            musteri4.TelefonNumarasi = 456743345;
+            musteri4.KimlikNumarasi = 4444444;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3, musteri4 };
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Adi);
+                Console.WriteLine(musteri.Soyadi);
+                Console.WriteLine(musteri.meslek);
+                Console.WriteLine(musteri.TelefonNumarasi);
+                Console.WriteLine("------------");
+
+            }
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Sil(musteri3);
+            musteriManager.Listele(musteri4);
+
+;
+
+        }
+    }
+}
